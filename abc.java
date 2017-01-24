afe8494508979949437c3ab33c06aa8e8a4f864c
Sree21 <sreekanth_telukuntla@yahoo.com>
2017-01-24 15:37:46 +0530

    Made a new java file to compare between two numbers

---
+++ abc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class abc {
+
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int a=scan.nextInt();
+		System.out.println("Enter the second number");
+		int b=scan.nextInt();
+		if(a>b)
+			System.out.println(a+" is larger");
+		else if(b>a)
+			System.out.println(b+" is larger");
+		else
+			System.out.println("These numbers are equal");	
+
+		System.out.println("Hello there");
+	}
+
+}
